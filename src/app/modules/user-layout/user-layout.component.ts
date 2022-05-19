1d9e96f0152098b1c965c3c7aa795db58bc2cc56
anton_vakhrushin <avvakhrushin@gmail.com>
2022-05-19 17:46:08 +0300

    QQ-15: user-layout styles

--- src/app/modules/user-layout/user-layout.component.ts
+++ src/app/modules/user-layout/user-layout.component.ts
@@ -1,10 +1,12 @@
-import { Observable } from 'rxjs';
+import { combineLatest, filter, map, Observable } from 'rxjs';
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { IMedia } from '@core/models/media.model';
 import { Nullable } from '@core/types/nullable.type';
 import { MediaFacade } from '@core/redux/media/media.facade';
 import { SetupImagesService } from './services/setup-images.service';
 import { MediaLoaderService } from './services/media-loader.service';
+
+const IMAGE_POSITION_OFFSET = 1;
 
 @Component({
   selector: 'app-user-layout',
@@ -14,15 +16,30 @@ import { MediaLoaderService } from './services/media-loader.service';
 })
 export class UserLayoutComponent implements OnInit {
   public readonly images$: Observable<Array<IMedia>>;
-  public selectedImageID$: Observable<Nullable<string>>;
+  public readonly selectedImage$: Observable<IMedia>;
+  public readonly currentImagePosition$: Observable<number>;
+  public readonly maxSafeNumber = Number.MAX_SAFE_INTEGER;
 
   constructor(
     mediaFacade: MediaFacade,
     private readonly setupImagesService: SetupImagesService,
     private readonly mediaLoaderService: MediaLoaderService
   ) {
-    this.images$ = mediaFacade.images$;
-    this.selectedImageID$ = mediaFacade.selectedImageID$;
+    this.images$ = combineLatest([
+      mediaFacade.images$.pipe(filter(images => !!images?.length)),
+      mediaFacade.selectedImagePosition$.pipe(filter(position => position !== null))
+    ]).pipe(
+      map(([images, position]) =>
+        images.slice(
+          Math.max(0, (position as number) - IMAGE_POSITION_OFFSET),
+          Math.min(images.length, (position as number) + IMAGE_POSITION_OFFSET + 1)
+        )
+      )
+    );
+    this.selectedImage$ = mediaFacade.selectedImage$.pipe(filter(Boolean));
+    this.currentImagePosition$ = combineLatest([this.images$, mediaFacade.selectedImageID$.pipe(filter(Boolean))]).pipe(
+      map(([images, selectedID]) => images.findIndex(({ id }) => id === selectedID))
+    );
   }
 
   public ngOnInit(): void {
